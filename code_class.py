d9bd4cdeec588edf31a16773d6a03c57104a8733
Awoodz <a.bois77@gmail.com>
2020-02-26 16:03:00 +0100

    User's first choice > OK

---
+++ code_class.py
@@ -0,0 +1,17 @@
+#CLASS
+
+def input_checker(choice_list) :
+    """ That function checks if the user entered a correct input """
+    while True :
+        user_input = input("Entrez le numéro de votre sélection : ")
+        try :
+            user_input = int(user_input)
+        except ValueError :
+            print("Ce n'est pas un chiffre !")
+            continue
+        if user_input < 1 or user_input > len(choice_list) :
+            print("Cet index n'existe pas")
+            continue
+        else :
+            break
+    return user_input
--- data.py
+++ data.py
@@ -1,3 +1,5 @@
 # DATA
 
 init_choice = ["1 - Quel aliment souhaitez-vous remplacer ?", "2 - Retrouver mes aliments subsitués"]
+
+init_answer = ["La liste des choix apparait !", "On récupère les anciennes infos"]
