979eacad82fc92b5b2aa0b2a1e42fd585cdf4c13
Awoodz <a.bois77@gmail.com>
2020-02-26 19:19:27 +0100

    Correcting GIT misuse

--- code.py
+++ code.py
@@ -4,34 +4,65 @@ from mysql.connector import Error
 from data import *
 from code_class import *
 
+cat_list = []
+ing_list = []
+vartest = "1"
+# display first request
 for elem in init_choice :
     print(elem)
 
-print(init_answer[input_checker(init_choice) - 1])
+#################################################
+#################################################
+#################################################
 
-
-# TEST CONNEXION MYSQL
-
-try :
-    connection = mysql.connector.connect (
+# Connexion to MySQL Server
+if input_checker(init_choice) == 1 :
+    try :
+        connection = mysql.connector.connect (
         host = 'localhost',
         database = 'testP5',
         user = 'testeur',
         password = 'openclassrooms'
     )
+        
+        if connection.is_connected():
+            cursor = connection.cursor()
 
-    if connection.is_connected():
-        db_Info = connection.get_server_info()
-        print("Connecté au serveur MySQL", db_Info)
-        cursor = connection.cursor()
-        cursor.execute("select database();")
-        record = cursor.fetchone()
-        print("Vous êtes connecté à la base de donnée ", record)
+# First Request - We want to displays categories of ingredients
+        cat_query = ('SELECT * FROM Categories')
+        cursor.execute(cat_query)
 
-except Error as e:
-    print("Une erreur est survenue, ", e)
-finally :
-    if (connection.is_connected()):
+# Displaying the list
+        print("Voici la liste des catégories :")
+        for (id, categorie) in cursor:
+            print("{} : {}" .format(id, categorie))
+            cat_list.append(categorie)
+        print(cat_list)
+
+        ing_query = (
+            'SELECT Aliments.aliment FROM Aliments INNER JOIN Categories ON Aliments.aliment_id = Categories.id WHERE Categories.id =' + str(input_checker(cat_list)) + ';'
+            )
+        cursor.execute(ing_query)
+        print("quelque chose s'est produit !")
+        for (aliment) in cursor:
+            ing_list.append(aliment)
+            print("{} : {}" .format(ing_list.index(aliment) + 1, aliment))
+            
+
+
+
+
+
         cursor.close()
-        connection.close()
-        print("Fini")        
+
+    except Error as e:
+        print("Une erreur est survenue, ", e)
+
+    finally :
+        if (connection.is_connected()):
+            cursor.close()
+            connection.close()
+            print("Fini")        
+
+else :
+    print("Le choix numéro 2 !")
